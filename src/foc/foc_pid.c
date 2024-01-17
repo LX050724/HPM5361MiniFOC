56d66a8fc1398860dfeb602009352f3f855271ef
LX050724 <yaojizhong@outlook.com>
2024-01-18 00:37:03 +0800

    修正增量式PID代码错误

--- src/foc/foc_pid.c
+++ src/foc/foc_pid.c
@@ -26,7 +26,7 @@ float foc_pid_controller(foc_pid_contrl_t *pid, float cur, float d, float exp)
 float foc_pid_increase_controller(foc_pid_contrl_t *pid, float cur, float exp)
 {
     float err = exp - cur;
-    float output = pid->kp * (err - pid->err[0]) + pid->ki * err + pid->kd * (err * 2.0f * pid->err[0] + pid->err[1]);
+    float output = pid->kp * (err - pid->err[0]) + pid->ki * err + pid->kd * (err - 2.0f * pid->err[0] + pid->err[1]);
     pid->err[1] = pid->err[0];
     pid->err[0] = err;
     return output;
